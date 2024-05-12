4e8685b97a09d76811bf5ec1921c57076b918872
Mashal Afzal <mashal.afzal2996@gmail.com>
2024-05-12 17:32:25 +0500

    Add files via upload

---
+++ task.js
@@ -0,0 +1,4 @@
+"use strict";
+function addition(num1, num2 = 20) { return num1 + num2; }
+let checked = addition(40, 60);
+console.log(checked);
---
+++ task.ts
@@ -0,0 +1,5 @@
+function addition(num1: number, num2: number=20)
+{return num1 + num2}
+
+let checked= addition(40,60)
+console.log (checked)
